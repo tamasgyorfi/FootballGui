adb2b82cd9e072cba82ee7aed99e77db7a29d02f
Tamas Gyorfi <gyorfitamas@gmail.com>
2018-07-04 13:47:40 +0100

    removed reactrix reference

--- src/components/panels/sidebar.js
+++ src/components/panels/sidebar.js
@@ -1,7 +1,6 @@
 import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
 import {Link} from 'react-router-dom'
-import "reactrix-sidebar/index.css";
 
 import matches from "../../img/matches.png"
 import league from "../../img/league.png"
